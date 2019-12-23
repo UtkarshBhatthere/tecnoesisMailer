1134edcfdf17277953aab45e02f3ba8a4438d880
utkarshbhatthere <utkarsh_bhatt@outlook.com>
2019-12-24 01:25:55 +0530

    Added DB sweep for contact fetch.

--- mailer.py
+++ mailer.py
@@ -2,24 +2,39 @@ import smtplib as smtp
 import ssl
 import email as mail
 import json
+import csv
 import mail_beautifier as mb
+import time
 
 # Fetching sender configuration.
 with open('assets/config.json') as cf:
     send_config = json.load(cf)
 
-print(send_config['ProvidorUrl'], send_config['Port'])
+# Set Attachments Here.
+attachments = ['assets/Brochure.pdf', 'assets/corporate_sponsorship_avenues.pdf']
 
+# Set Mail Draft Settings in file 'draft_config.json'
 with open('assets/draft_config.json') as df:
     draft_config = json.load(df)
 
 # Fetching password for given Email ID.
 password = input(f"Enter Password for {draft_config['sender']}\n")
 
-# Populating default context for SSL.
-ssl_context = ssl.create_default_context()
+# Fetch Mail body.
+with open("assets/mail.txt") as mail:
+    mail_str = mail.read()
 
-with smtp.SMTP_SSL(send_config['ProvidorUrl'], send_config['Port'], context=ssl_context) as server:
-    message = mb.prepare_draft(draft_config, "This is a Test mail.", "assets/sample.pdf")
-    server.login(draft_config['sender'], password)
-    server.sendmail(draft_config['sender'], [draft_config['receiver'], draft_config['bcc']], message.as_string())
+with open("assets/test_db.csv") as contacts_file:
+    contacts = csv.reader(contacts_file)
+
+    for contact in contacts:    # Fetching Individual contacts.
+        mail_body = mail_str.format(contact[0]) # Fill in the name of the Contact accordingly.
+
+        # Populating default context for SSL.
+        ssl_context = ssl.create_default_context()
+
+        with smtp.SMTP_SSL(send_config['ProvidorUrl'], send_config['Port'], context=ssl_context) as server:
+            message = mb.prepare_draft(draft_config, mail_body, attachments)
+            server.login(draft_config['sender'], password)
+            server.sendmail(draft_config['sender'], [contact[1], contact[2]], message.as_string())
+            time.sleep(1)
